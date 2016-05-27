e858b14104a2db87e520aa8a8c3fc5fa5b17da85
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-05-27 11:13:16 +0200

    [ClientMain.java]Iniziata implementazione di chat multi-client

--- model/ClientMain.java
+++ model/ClientMain.java
@@ -1,9 +1,12 @@
 package model;
 
+import chatUtils.data.Chat;
 import chatUtils.net.Talker;
 import chatUtils.data.Consts;
 import chatUtils.data.ChatMessage;
+import chatUtils.data.UserData;
 import java.net.InetAddress;
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import utils.net.SocketHandler;
@@ -28,10 +31,14 @@ public class ClientMain {
 //            Logger.getLogger(ClientMain.class.getName()).log(Level.SEVERE, null, ex);
 //        }
         
+        String userName = new Scanner(System.in).nextLine();
         socketHandler = new SocketHandler(inetAddress, Consts.PORT);
         streamHandler = new StreamHandler(socketHandler.getSocket());
         streamHandler.init();
-        chatMessage = new ChatMessage("Ryther");
+        UserData userData = new UserData(userName);
+        userData.addChat(new Chat("Prova"));
+        streamHandler.pushToStream(userData);
+        chatMessage = new ChatMessage(userName);
         ExecutorService threadPool = Executors.newFixedThreadPool(Consts.TALKER_THREADS);
         Runnable writingThread = new Talker(streamHandler, chatMessage);
         Runnable readingThread = new Talker(streamHandler);
