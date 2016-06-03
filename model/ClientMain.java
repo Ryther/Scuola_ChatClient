ad40ec99a47b503d4456cc896fe43cffe9fd0c0c
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-06-03 17:46:27 +0200

    [ClientMain.java]Primo tentativo di utilizzo NIO

--- model/ClientMain.java
+++ model/ClientMain.java
@@ -1,14 +1,11 @@
 package model;
 
 import chatUtils.data.Chat;
-import chatUtils.net.Talker;
 import chatUtils.data.Consts;
 import chatUtils.data.ChatMessage;
 import chatUtils.data.UserData;
-import java.net.InetAddress;
 import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import utils.net.SocketChannelHandler;
 import utils.net.SocketHandler;
 import utils.net.StreamHandler;
 
@@ -22,27 +19,50 @@ public class ClientMain {
     private static StreamHandler streamHandler;
     private static ChatMessage chatMessage;
     
+    private static SocketChannelHandler socketChannelHandler;
+    
+//    public static void main(String[] args) {
+//        
+//        System.out.print("Inserire nickname: ");
+//        String userName = new Scanner(System.in).nextLine();
+//        socketHandler = new SocketHandler(Consts.INETADDRESS, Consts.PORT);
+//        streamHandler = new StreamHandler(socketHandler.getSocket());
+//        streamHandler.init();
+//        UserData userData = new UserData(userName);
+//        System.out.print("Inserire chat a cui connettersi: ");
+//        String chatName = new Scanner(System.in).nextLine();
+//        userData.addChat(new Chat(chatName));
+//        streamHandler.pushToStream(userData);
+//        chatMessage = new ChatMessage(userName);
+//        chatMessage.setChatName(chatName);
+//        ExecutorService threadPool = Executors.newFixedThreadPool(Consts.TALKER_THREADS);
+//        Runnable writingThread = new Talker(streamHandler, chatMessage);
+//        Runnable readingThread = new Talker(streamHandler);
+//        threadPool.submit(writingThread);
+//        threadPool.submit(readingThread);
+//    }
+    
     public static void main(String[] args) {
-        
-        InetAddress inetAddress = null;
-        inetAddress = InetAddress.getLoopbackAddress();
-
         System.out.print("Inserire nickname: ");
         String userName = new Scanner(System.in).nextLine();
-        socketHandler = new SocketHandler(inetAddress, Consts.PORT);
-        streamHandler = new StreamHandler(socketHandler.getSocket());
-        streamHandler.init();
+        
         UserData userData = new UserData(userName);
+        
         System.out.print("Inserire chat a cui connettersi: ");
         String chatName = new Scanner(System.in).nextLine();
         userData.addChat(new Chat(chatName));
-        streamHandler.pushToStream(userData);
+        
+        socketChannelHandler = new SocketChannelHandler(Consts.INETADDRESS, Consts.PORT);
+
         chatMessage = new ChatMessage(userName);
-        chatMessage.setChatName(chatName);
-        ExecutorService threadPool = Executors.newFixedThreadPool(Consts.TALKER_THREADS);
-        Runnable writingThread = new Talker(streamHandler, chatMessage);
-        Runnable readingThread = new Talker(streamHandler);
-        threadPool.submit(writingThread);
-        threadPool.submit(readingThread);
+
+        while (true) {
+
+            chatMessage.setMessage(new Scanner(System.in).nextLine());
+            chatMessage.setDateTime();
+            socketChannelHandler.pushToChannel(chatMessage);
+        }
+        
+//        socketChannelHandler.close();
     }
 }
